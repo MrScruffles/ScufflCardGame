45892cf34940144ec2a49487121cd1529324ef0a
Ash <83662366+MrScruffles@users.noreply.github.com>
2021-05-20 09:43:52 -0400

    Update request.js

--- WeatherModule/request.js
+++ WeatherModule/request.js
@@ -1,6 +1,6 @@
 const key = 'cea902c2bfd3adf64a0a3d36565e1ac7';
 
-// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cea902c2bfd3adf64a0a3d36565e1ac7';
+// const baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=cea902c2bfd3adf64a0a3d36565e1ac7';
 
 // fetch(baseURL)
 //     .then((data) => { console.log('response', data.json()) })

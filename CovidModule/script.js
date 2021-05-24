70c2ad59745d6dbe63a2a2abe26901d346441667
Ash <83662366+MrScruffles@users.noreply.github.com>
2021-05-24 09:45:43 -0400

    Create script.js

---
+++ CovidModule/script.js
@@ -0,0 +1,15 @@
+fetch('https://corona.lmao.ninja/v2/countries/USA')
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  console.log(data);
+  document.getElementById("country").innerHTML = data.country;
+  document.getElementById("active").innerHTML = data.active.toLocaleString();
+  document.getElementById("cases").innerHTML = data.cases.toLocaleString();
+  document.getElementById("critical").innerHTML = data.critical.toLocaleString();
+  document.getElementById("death").innerHTML = data.deaths.toLocaleString();
+  document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
+  document.getElementById("tests").innerHTML = data.tests.toLocaleString();
+  document.getElementById("flag").src = data.countryInfo.flag;
+});
